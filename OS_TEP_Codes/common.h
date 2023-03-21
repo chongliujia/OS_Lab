2e70fdf7992ad9263f8a0dbf035433c308b9b74a
chongliujia <jcl314159@gmail.com>
2023-03-22 03:56:26 +0800

    add some codes

--- OS_TEP_Codes/common.h
+++ OS_TEP_Codes/common.h
@@ -5,6 +5,8 @@
 #include <sys/stat.h>
 #include <assert.h>
 
+#include <pthread.h>
+
 double GetTime() {
     struct timeval t;
     int rc = gettimeofday(&t, NULL);
@@ -15,6 +17,32 @@ double GetTime() {
 void Spin(int howlong) {
     double t = GetTime();
     while ((GetTime() - t) < (double) howlong);
+}
+
+void Pthread_create(pthread_t *t, const pthread_attr_t *attr, 
+                    void *(*start_routine)(void *), void *arg) {
+    int rc = pthread_create(t, attr, start_routine, arg);
+    assert(rc == 0);
+}
+
+void Pthread_join(pthread_t thread, void **value_ptr) {
+    int rc = pthread_join(thread, value_ptr);
+    assert(rc == 0);
+}
+
+void Pthread_mutex_lock(pthread_mutex_t *mutex) {
+    int rc = pthread_mutex_lock(mutex);
+    assert(rc == 0);
+}
+
+void Pthread_mutex_unlock(pthread_mutex_t *mutex) {
+    int rc = pthread_mutex_unlock(mutex);
+    assert(rc == 0);
+}
+
+void Pthread_mutex_init(pthread_mutex_t *mutex, pthread_mutexattr_t *attr) {
+    int rc = pthread_mutex_init(mutex, attr);
+    assert(rc == 0);
 }
 
 #endif
